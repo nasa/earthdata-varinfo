498d30d9334617bc86e3a5cced92684747efad0f
Owen Littlejohns <owen.m.littlejohns@nasa.gov>
2023-09-15 01:25:44 -0400

    DAS-1895 - Make generate_collection_umm_var accessible from the top level of the package.

--- varinfo/__init__.py
+++ varinfo/__init__.py
@@ -1,4 +1,5 @@
 """ Module containing convenience exports for the earthdata-varinfo library."""
 from .cf_config import CFConfig  # noqa
+from .generate_umm_var import generate_collection_umm_var  # noqa
 from .var_info import VarInfoFromDmr, VarInfoFromNetCDF4  # noqa
 from .variable import VariableFromDmr, VariableFromNetCDF4  # noqa
